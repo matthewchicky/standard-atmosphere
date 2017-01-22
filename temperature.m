3c8ea2b192761db9e88b68c7391d09aa4038a349
matthewchicky <matthewchicky@users.noreply.github.com>
2017-01-22 15:47:33 -0500

    Added comments

--- temperature.m
+++ temperature.m
@@ -1,7 +1,24 @@
 function [ T ] = temperature( Z )
-% This is an unfinished function. It will calculate the temperature profile
-% of the U.S. Standard Atmosphere from mean sea level to 11 km geometric
-% height.
+%% Function Description & Creation Info
+%   This function calculates the atmospheric temperature given geometric height (altitude). 
+%   Calculation is based on the U.S. Standard Atmosphere. Valid in the geopotential range of
+%   0 km to 11 km altitude.
+
+%   U.S. Standard Atmosphere, 1976:
+%   <https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/19770009539.pdf>
+
+%   Geopotential to Geometric Altitude Conversion:
+%   <http://www.pdas.com/geopot.pdf>
+
+%   Created by:     Matthew Chicky
+%   Last Modified:  1/22/17, 14:26:00 EST
+
+%% Variable Description
+
+%% Variable Declaration
+
+%% Compute Temperature
+
+
 
 end
-
